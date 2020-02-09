ff320784bd272b5773fdbaff13377d644e42277d
Witaliikaplun <vitalijkaplun@gmail.com>
2020-02-09 20:03:02 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class Controller {
+
+    @FXML
+    private TextField textFiled;
+    @FXML
+    private TextArea textArea;
+    private Object KeyEvent;
+
+    public void onClick(ActionEvent actionEvent) {
+        send();
+    }
+
+
+    public void onEnter(javafx.scene.input.KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) send();
+    }
+
+    private void send() {
+        if (!textFiled.getText().equals("")) {   //проверка на пустой текст
+            textArea.appendText(textFiled.getText() + "\n");
+            textFiled.clear();
+        }
+    }
+}
